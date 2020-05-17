f9edc81f7fc43e3b98483cf37ca9009751f62f28
francesco <vannini@gmail.com>
2020-05-17 20:20:03 +0200

    Added power on prompt

--- upload.sh
+++ upload.sh
@@ -5,17 +5,18 @@ else
 	last="1970-01-01 00:00"
 fi
 
-cd src
-nodemcu-uploader file remove init.lua
+read -p "Power on the board, then press ENTER immediately after"
 
+cd src
+nodemcu-uploader file remove init.lua || exit 1
 if [ "$1" == "-i" ]; then
 	list=$(find *.lua ! -name "init.lua" -newermt "$last" -printf "%f ")
 	list=$list"init.lua"
 	echo "Uploading $list"
-	nodemcu-uploader --baud 921600 --timeout 60 upload $list
+	nodemcu-uploader --baud 921600 --timeout 60 upload $list || exit 1
 else
-	nodemcu-uploader --baud 921600 --timeout 60 upload *
 	echo "Uploading all files in src/"
+	nodemcu-uploader --baud 921600 --timeout 60 upload * || exit 1
 fi
 cd ..
 date "+%F %T" > "upload.lst"
