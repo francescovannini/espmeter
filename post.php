a0de8972848dcddb7d1b26262972729bcd43f3c8
francesco <vannini@gmail.com>
2020-08-30 10:50:04 +0200

    Restored microtime()

--- post.php
+++ post.php
@@ -5,9 +5,16 @@
 	fwrite($fp, date('m/d/Y h:i:s a', time()) . ' - ' . $body . chr(10));
 	fclose($fp);
 
+<<<<<<< HEAD
 	$response = array(
 		"time" => time()
 	);
+=======
+// Provides time sync to device
+$response = array(
+	"time" => microtime(true)
+);
+>>>>>>> d9c4b96... Restored microtime()
 
 	header('Content-Type: application/json');
 	print(json_encode($response));
