0a46be880cb801af483fe620a30f45c2896fca71
francesco <vannini@gmail.com>
2020-10-10 19:41:53 +0200

    Added view

--- espmeter.sql
+++ espmeter.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.17  Distrib 10.3.23-MariaDB, for debian-linux-gnueabi (armv7l)
 --
--- Host: localhost    Database: espmeter
+-- Host: localhost    Database: gascounter
 -- ------------------------------------------------------
 -- Server version	10.3.23-MariaDB-0+deb10u1
 
@@ -43,8 +43,22 @@ CREATE TABLE `log` (
   `ts` datetime NOT NULL DEFAULT current_timestamp(),
   `timedrift` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary table structure for view `pulses_over_time`
+--
+
+DROP TABLE IF EXISTS `pulses_over_time`;
+/*!50001 DROP VIEW IF EXISTS `pulses_over_time`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `pulses_over_time` (
+  `ts` tinyint NOT NULL,
+  `pulses` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `vcc`
@@ -62,6 +76,25 @@ CREATE TABLE `vcc` (
   CONSTRAINT `fk_vcc_1` FOREIGN KEY (`idlog`) REFERENCES `log` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `pulses_over_time`
+--
+
+/*!50001 DROP TABLE IF EXISTS `pulses_over_time`*/;
+/*!50001 DROP VIEW IF EXISTS `pulses_over_time`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`gascounter`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `pulses_over_time` AS select `l`.`ts` + interval (`c`.`slot` * 5) minute AS `ts`,`c`.`pulses` AS `pulses` from (`log` `l` join `counter` `c` on(`l`.`id` = `c`.`idlog`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -72,4 +105,4 @@ CREATE TABLE `vcc` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-30 10:52:56
+-- Dump completed on 2020-10-10 19:39:01
