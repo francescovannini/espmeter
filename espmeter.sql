9da0b18783c7174825200bb749dd1524fc8f9b84
francesco <vannini@gmail.com>
2020-11-22 19:04:28 +0100

    Added views

--- espmeter.sql
+++ espmeter.sql
@@ -1,72 +1,23 @@
--- MySQL dump 10.17  Distrib 10.3.23-MariaDB, for debian-linux-gnueabi (armv7l)
---
--- Host: localhost    Database: gascounter
--- ------------------------------------------------------
--- Server version	10.3.23-MariaDB-0+deb10u1
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ts` datetime NOT NULL DEFAULT current_timestamp(),
+  `timedrift` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `counter`
---
 
 DROP TABLE IF EXISTS `counter`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `counter` (
   `idlog` int(11) NOT NULL,
   `slot` smallint(6) NOT NULL,
   `pulses` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`idlog`,`slot`),
   CONSTRAINT `fk_counter_1` FOREIGN KEY (`idlog`) REFERENCES `log` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
---
--- Table structure for table `log`
---
-
-DROP TABLE IF EXISTS `log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ts` datetime NOT NULL DEFAULT current_timestamp(),
-  `timedrift` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Temporary table structure for view `pulses_over_time`
---
-
-DROP TABLE IF EXISTS `pulses_over_time`;
-/*!50001 DROP VIEW IF EXISTS `pulses_over_time`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE TABLE `pulses_over_time` (
-  `ts` tinyint NOT NULL,
-  `pulses` tinyint NOT NULL
-) ENGINE=MyISAM */;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `vcc`
---
 
 DROP TABLE IF EXISTS `vcc`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vcc` (
   `idlog` int(11) NOT NULL,
   `ts` datetime NOT NULL,
@@ -74,35 +25,34 @@ CREATE TABLE `vcc` (
   PRIMARY KEY (`ts`,`idlog`),
   KEY `fk_vcc_1_idx` (`idlog`),
   CONSTRAINT `fk_vcc_1` FOREIGN KEY (`idlog`) REFERENCES `log` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
---
--- Final view structure for view `pulses_over_time`
---
 
-/*!50001 DROP TABLE IF EXISTS `pulses_over_time`*/;
-/*!50001 DROP VIEW IF EXISTS `pulses_over_time`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`gascounter`@`%` SQL SECURITY DEFINER */
-/*!50001 VIEW `pulses_over_time` AS select `l`.`ts` + interval (`c`.`slot` * 5) minute AS `ts`,`c`.`pulses` AS `pulses` from (`log` `l` join `counter` `c` on(`l`.`id` = `c`.`idlog`)) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+DROP VIEW IF EXISTS `pulses_over_time`;
+CREATE VIEW `pulses_over_time` AS
+    SELECT 
+        `l`.`ts` + INTERVAL (`c`.`slot` * 5) MINUTE AS `ts`,
+        `c`.`pulses` AS `pulses`
+    FROM
+        (`log` `l`
+        JOIN `counter` `c` ON (`l`.`id` = `c`.`idlog`));
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-10 19:39:01
+DROP VIEW IF EXISTS `pulses_over_day`;
+CREATE VIEW `pulses_over_day` AS
+    SELECT 
+        CAST(`pulses_over_time`.`ts` AS DATE) AS `ts`,
+        SUM(`pulses_over_time`.`pulses`) AS `pulses`
+    FROM
+        `pulses_over_time`
+    GROUP BY CAST(`pulses_over_time`.`ts` AS DATE);
+
+
+DROP VIEW IF EXISTS `pulses_over_hour`;
+CREATE VIEW `pulses_over_hour` AS
+    SELECT 
+        CAST(`pulses_over_time`.`ts` AS DATE) + INTERVAL HOUR(`pulses_over_time`.`ts`) HOUR AS `ts`,
+        SUM(`pulses_over_time`.`pulses`) AS `pulses`
+    FROM
+        `pulses_over_time`
+    GROUP BY CAST(`pulses_over_time`.`ts` AS DATE) , HOUR(`pulses_over_time`.`ts`);
