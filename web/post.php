490bf8f0dedf8d13d47cfcab28daa34ba78f9fb0
francesco <vannini@gmail.com>
2020-12-26 18:57:04 +0100

    Fixed bad merge

--- web/post.php
+++ web/post.php
@@ -1,31 +1,22 @@
 <?php
-<<<<<<< HEAD
-	
-	$body = file_get_contents('php://input');
-=======
 
 require_once("parsetodb.php");
 
 $body = file_get_contents('php://input');
 
 if (strlen($body) > 0) {
->>>>>>> 2e9d110... Bugfixes
 	$fp = fopen('request.log', 'a');
-	fwrite($fp, date('m/d/Y h:i:s a', time()) . ' - ' . $body . chr(10));
+	fwrite($fp, date('c', time()) . ' - ' . $body . chr(10));
 	fclose($fp);
+	parse_log($body);
+}
 
-<<<<<<< HEAD
-	$response = array(
-		"time" => time()
-	);
-=======
 // Provides time sync to device
 $response = array(
 	"time" => microtime(true)
 );
->>>>>>> d9c4b96... Restored microtime()
 
-	header('Content-Type: application/json');
-	print(json_encode($response));
-	
+header('Content-Type: application/json');
+print(json_encode($response));
+
 ?>
